34fa75603434e55431f2307894ed12dafcb59cdd
Dave Ceddia <dceddia@gmail.com>
2014-10-24 12:51:29 -0400

    styling for unknown, open, and closed states

--- public/app.js
+++ public/app.js
@@ -1,6 +1,16 @@
 var app = angular.module('GarageDoor', []);
 
 app.controller("GarageCtrl", function($scope) {
-  $scope.doorImage = "images/unknown.png";
   $scope.doorState = "unknown";
+  $scope.doorState = "open";
+  $scope.doorState = "closed";
+  $scope.statusIcon = function() {
+    switch($scope.doorState) {
+      case 'open':
+        return "fa-warning";
+      case 'closed':
+        return "fa-check";
+    }
+    return "fa-question";
+  };
 });
