9d5a5729cc7c68a9125e074c65fdea66db20d2be
Dave Ceddia <dceddia@gmail.com>
2014-10-24 23:36:37 -0400

    reorg #2

--- app.js
+++ app.js
@@ -7,7 +7,12 @@ var bodyParser = require('body-parser');
 
 
 var app = express();
-var server = require('http').Server(app);
+
+app.set('port', process.env.PORT || 3000);
+
+var server = app.listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + server.address().port);
+});
 io = require('socket.io')(server);
 
 
@@ -66,10 +71,3 @@ app.use(function(err, req, res, next) {
 
 
 module.exports = app;
-
-
-app.set('port', process.env.PORT || 3000);
-
-var server = app.listen(app.get('port'), function() {
-  console.log('Express server listening on port ' + server.address().port);
-});
