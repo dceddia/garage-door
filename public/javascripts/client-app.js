24c559e1dfb9394aa443ee96d1a8c1b6a87a5c08
Dave Ceddia <dceddia@gmail.com>
2014-10-26 10:38:38 -0400

    refactored, added button

--- public/javascripts/client-app.js
+++ public/javascripts/client-app.js
@@ -1,29 +1,78 @@
-var app = angular.module('GarageDoor', ['btford.socket-io']);
+var app = angular.module('GarageMonitor', ['btford.socket-io']);
 
 app.factory('doorSocket', function(socketFactory) {
   return socketFactory();
 });
 
-app.controller("GarageCtrl", function($scope, doorSocket, $http) {
+app.controller("GarageCtrl", function($scope, GarageDoor) {
   $scope.doorState = "unknown";
 
-  $scope.statusIcon = function() {
-    switch($scope.doorState) {
+  $scope.withState = function(state, ifOpen, ifClosed, ifOther) {
+    var choice = ifOther;
+    switch(state) {
       case 'open':
-        return "fa-warning";
+        choice = ifOpen;
+        break;
       case 'closed':
-        return "fa-check";
+        choice = ifClosed;
+        break;
     }
-    return "fa-question";
+
+    // If it's a function, call it
+    typeof choice === 'function' && choice();
+
+    return choice;
+  };
+
+  $scope.statusIcon = function() {
+    return $scope.withState($scope.doorState, "fa-warning", "fa-check", "fa-question");
+  };
+
+  $scope.buttonText = function() {
+    return $scope.withState($scope.doorState, "Close It", "Open It", "Trigger It");
+  };
+
+  $scope.changeDoorState = function() {
+    $scope.withState($scope.doorState, GarageDoor.close, GarageDoor.open, GarageDoor.trigger);
   };
 
   // When the door state changes, the server will send us an update
-  doorSocket.on('change', function(newDoorState) {
+  GarageDoor.onStatusChange(function(newDoorState) {
     $scope.doorState = newDoorState;
   });
 
-  // Ask the server for the current door status
-  $http.get('/status').then(function(res) {
-    $scope.doorState = res.data;
+  // On initialization, ask the server for the current door status
+  GarageDoor.getStatus().then(function(status) {
+    $scope.doorState = status;
   });
+});
+
+app.service("GarageDoor", function($http, doorSocket) {
+  function extractStatus(res) {
+    return res.data;
+  }
+
+  var methods = {
+    onStatusChange: function(callbackFn) {
+      doorSocket.on('change', callbackFn);
+    },
+
+    getStatus: function() {
+      return $http.get('/status').then(extractStatus);
+    },
+
+    open: function() {
+      return $http.post('/status/change');
+    },
+
+    close: function() {
+      return $http.post('/status/change');
+    },
+
+    trigger: function() {
+      return $http.post('/status/change');
+    }
+  };
+
+  return methods;
 });
